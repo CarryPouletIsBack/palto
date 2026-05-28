76bcac43168a93236ef97cdc15c6e35dc8e183f6
Anthony Merault <merault.anthony@gmail.com>
2026-05-28 14:44:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1004,8 +1004,6 @@ function App() {
         currentPage !== 'dashboard-navigation' &&
         currentPage !== 'client-compte' &&
         currentPage !== 'client-meet-driver' &&
-        currentPage !== 'accueil' &&
-        currentPage !== 'accueil-chauffeur' &&
         !currentPage.startsWith('destination-') ? (
         <Header onContactClick={handleContactClick} onLogoClick={handleLogoClick} />
       ) : null}
